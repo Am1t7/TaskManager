e4e2b7cacdac9c81fa638ca3aad644f57a489d12
Am1t7 <fsdf>
2022-02-10 12:02:46 +0200

    fixed data base start making the main client and the main server now added :)

---
+++ mainClient.py
@@ -0,0 +1,26 @@
+import client_com
+import client_pro
+import clientGraphic
+import queue
+import setting
+import wx
+import threading
+
+
+
+
+
+
+def main_loop():
+    while True:
+        data = msg_q.get()
+
+
+msg_q = queue.Queue()
+comm = client_com.Client_com(setting.SERVER_IP,setting.SERVER_PORT,msg_q)
+threading.Thread(target=main_loop, args=(msg_q,comm,)).start()
+
+
+app = wx.App(False)
+frame = clientGraphic.MainFrame()
+app.MainLoop()
