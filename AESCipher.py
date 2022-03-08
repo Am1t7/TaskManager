f796587a0818949febd664a8f10cfe7b9833351e
amit <finder.amit@gmail.com>
2022-03-08 09:42:07 +0200

    crypto

---
+++ AESCipher.py
@@ -0,0 +1,48 @@
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+import hashlib
+
+class AESCipher(object):
+
+    def __init__(self, key):
+        """
+        constructor to AESCipher
+        :param key: the key to decrypt and encrypt
+        """
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        """
+        takes a string and encrypts it
+        :param raw: the msg
+        :return: the msg encrypted
+        """
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+
+    def decrypt(self, enc):
+        """
+        gets a msg and decrypts it
+        :param enc: encrypted msg (
+        :return: decrypted msg
+        """
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        """
+
+        :param s: String
+        :return: makes it 64 bits
+        """
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
---
+++ RSAClass.py
@@ -0,0 +1,33 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+class RSAClass:
+    def __init__(self):
+        self._keyPair = None
+        self._generate_keys()
+
+    def _generate_keys(self):
+        '''
+            no input.
+            generate the RSA couple keys
+        :return:
+        '''
+        self._keyPair = RSA.generate(3072)
+
+    def get_public_key_pem(self):
+        pubkey = self._keyPair.publickey()
+        pubkeyPEM = pubkey.exportKey()
+        return pubkeyPEM
+
+    def decrypt_msg(self, encdata):
+        private_key = PKCS1_OAEP.new(self._keyPair)
+        return private_key.decrypt(encdata)
+
+
+
+
+def encrypt_msg(data, pubkeyPEM):
+    pubkey = RSA.import_key(pubkeyPEM)
+    encryptor = PKCS1_OAEP.new(pubkey)
+    return encryptor.encrypt(data.encode())
