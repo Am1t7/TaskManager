158606202c221c823342992193802729507620b8
amit <finder.amit@gmail.com>
2022-03-21 15:10:19 +0200

    start comments :)

--- controller.py
+++ controller.py
@@ -17,25 +17,28 @@ class ProcThread(Thread):
     """
 
     #----------------------------------------------------------------------
-    def __init__(self):#,cpu,mem,disk):
-        #self.cpu_lim = cpu
-        #self.mem_lim = mem
-        #self.disk_lim = disk
+    def __init__(self):
         self.mac = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)for ele in range(0, 8 * 6, 8)][::-1]).upper()
         """Constructor"""
         Thread.__init__(self)
         self.start() 
 
     def get_cpu(self,p):
-
+        '''
+        get the cpu of the proc
+        :param p: the proc
+        :return:
+        '''
         cpu = p.as_dict(['cpu_times'])
-        cpu_percent = cpu['cpu_times'].user #+ \
-                      #cpu['cpu_times'].children_user
-                      #cpu['cpu_times'].system + \
-                      #cpu['cpu_times'].children_system
+        cpu_percent = cpu['cpu_times'].user
         return cpu_percent/100.0
 
     def get_disk(self,p):
+        '''
+        get the disk usage of the proc
+        :param p: the process
+        :return:
+        '''
         io_counters = p.io_counters()
         disk_usage_process = io_counters[2] + io_counters[3]  # read_bytes + write_bytes
         disk_io_counter = psutil.disk_io_counters()
@@ -44,15 +47,18 @@ class ProcThread(Thread):
 
     #----------------------------------------------------------------------
     def run(self):
-        """"""
+        '''
+        main thread
+        :return:
+        '''
         procs = []
         bad_procs = []
-        already_checked = []
         cpu_percent = 0
         mem_percent = 0
         disk_percent = 0
         count = 0
         db = DB()
+        # get the processes
         for proc in psutil.process_iter():
             try:
                 p = psutil.Process(proc.pid)
